c5bfbb3b5ea4090caf7b207fad64b44809f001b3
Donny Kurnia <donnykurnia@gmail.com>
2015-08-10 10:01:15 +0700

    add compileInsertGetId and processInsertGetId method taken from PostgresGrammar and PostgresProcessor classes as-is I've check it in my project using php artisan tinker and it's working well PS: this library need tests to make sure all methods working well
    Signed-off-by: Donny Kurnia <donnykurnia@gmail.com>

--- src/Firebird/Query/Processors/FirebirdProcessor.php
+++ src/Firebird/Query/Processors/FirebirdProcessor.php
@@ -1,7 +1,29 @@
 <?php namespace Firebird\Query\Processors;
 
 use Illuminate\Database\Query\Processors\Processor;
+use Illuminate\Database\Query\Builder;
 
 class FirebirdProcessor extends Processor {
+  /**
+   * Process an "insert get ID" query.
+   *
+   * @param  \Illuminate\Database\Query\Builder  $query
+   * @param  string  $sql
+   * @param  array   $values
+   * @param  string  $sequence
+   * @return int
+   */
+  public function processInsertGetId(Builder $query, $sql, $values, $sequence = null)
+  {
+    $results = $query->getConnection()->selectFromWriteConnection($sql, $values);
+
+    $sequence = $sequence ?: 'id';
+
+    $result = (array) $results[0];
+
+    $id = $result[$sequence];
+
+    return is_numeric($id) ? (int) $id : $id;
+  }
 
 }
