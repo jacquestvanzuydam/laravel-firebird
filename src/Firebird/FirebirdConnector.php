e877b297992fc19d78a5f380b27eedf80437dde0
Asım TAŞ <risperdal@yandex.com>
2015-10-11 23:47:34 +0300

    fix missing charset option

--- src/Firebird/FirebirdConnector.php
+++ src/Firebird/FirebirdConnector.php
@@ -16,17 +16,17 @@ class FirebirdConnector extends Connector implements ConnectorInterface {
    */
   public function connect(array $config)
   {
-    $options = $this->getOptions($config);
+$options = $this->getOptions($config);
 
     $path = $config['database'];
-
+    $charset = $config['charset'];
     $host = $config['host'];
     if ( empty($host))
     {
       throw new InvalidArgumentException("Host not given, required.");
     }
 
-    return $this->createConnection("firebird:dbname={$host}:{$path}", $config, $options);
+    return $this->createConnection("firebird:dbname={$host}:{$path};charset={$charset}", $config, $options);
   }
 
 }
