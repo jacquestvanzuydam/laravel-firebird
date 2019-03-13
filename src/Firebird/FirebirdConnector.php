db7d07a7d3ef9c444a6ed7e2f9aadc004fe2290b
luccaheberle <heberle@srssistemas.com>
2019-03-13 10:57:01 -0300

    Identation fix

--- src/Firebird/FirebirdConnector.php
+++ src/Firebird/FirebirdConnector.php
@@ -31,7 +31,7 @@ class FirebirdConnector extends Connector implements ConnectorInterface {
       throw new InvalidArgumentException("Host not given, required.");
     }
 
-	return $this->createConnection("firebird:dbname={$host}:{$path};charset={$charset};role={$role}", $config, $options);
+    return $this->createConnection("firebird:dbname={$host}:{$path};charset={$charset};role={$role}", $config, $options);
   }
 
 }
